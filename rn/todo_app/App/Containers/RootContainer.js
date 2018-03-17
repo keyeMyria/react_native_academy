d8fc9881f6d91e8ccc59c67b05e5553e12514cb4
rszalski <radoslaw.szalski@gmail.com>
2018-03-17 16:12:10 +0100

    Removes unnecessary boilerplate/examples

--- rn/todo_app/App/Containers/RootContainer.js
+++ rn/todo_app/App/Containers/RootContainer.js
@@ -5,7 +5,6 @@ import { connect } from 'react-redux'
 import StartupActions from '../Redux/StartupRedux'
 import ReduxPersist from '../Config/ReduxPersist'
 
-// Styles
 import styles from './Styles/RootContainerStyles'
 
 class RootContainer extends Component {
