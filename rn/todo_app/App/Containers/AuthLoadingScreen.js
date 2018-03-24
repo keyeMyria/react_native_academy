620c312f97777a9c59b57013c5f546cf282eba3a
rszalski <radoslaw.szalski@gmail.com>
2018-03-24 10:58:49 +0100

    Reads jwtToken from persistStore

--- rn/todo_app/App/Containers/AuthLoadingScreen.js
+++ rn/todo_app/App/Containers/AuthLoadingScreen.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import { ActivityIndicator, AsyncStorage } from 'react-native'
+import { connect } from 'react-redux'
 
-export default class AuthLoadingScreen extends React.Component {
+class AuthLoadingScreen extends React.Component {
   constructor (props) {
     super(props)
-    this._initializeAuth()
-  }
 
-  _initializeAuth = async () => {
-    const userToken = await AsyncStorage.getItem('jwtToken');
-    this.props.navigation.navigate(userToken ? 'App' : 'Auth')
+    this.props.navigation.navigate(this.props.jwtToken ? 'App' : 'Auth')
   }
 
   render () {
     return (
-      <ActivityIndicator animating={true} size="small" color="#00ff00" />
+        <ActivityIndicator animating={true} size="small" color="#00ff00" />
     )
   }
 }
+
+const mapStateToProps = (state) => ({
+  jwtToken: state.todo.jwtToken
+})
+export default connect(mapStateToProps)(AuthLoadingScreen)
